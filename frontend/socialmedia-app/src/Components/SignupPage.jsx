809b31faf632e916c176c68d599e2042e883ab74
ShivaSingh17121997 <shivasingh001997@gmail.com>
2023-07-17 14:24:16 +0530

    added

--- frontend/socialmedia-app/src/Components/SignupPage.jsx
+++ frontend/socialmedia-app/src/Components/SignupPage.jsx
@@ -1,7 +1,7 @@
 import React, { useReducer } from 'react'
 import axios from "axios"
 let initalstate={
-name:"",email:"",gender:"",password:"",is_married:"",city:"",age:""
+name:"",email:"",gender:"",password:""
 }
 const reducer=(state=initalstate,action)=>{
 
@@ -30,24 +30,9 @@ const reducer=(state=initalstate,action)=>{
                 gender:action.payload
             }
         }
-        case "Age" :{
-            return {
-                ...state,
-                age:action.payload
-            }
-        }
-        case "City" :{
-            return {
-                ...state,
-                city:action.payload
-            }
-        }
-        case "is_married" :{
-            return {
-                ...state,
-                is_married:action.payload
-            }
-        }
+       
+       
+      
         default:{
             return state
         }
@@ -55,7 +40,7 @@ const reducer=(state=initalstate,action)=>{
 }
 export const SignupPage = () => {
     let [state,dispatch]=useReducer(reducer,initalstate)
-    const {name,email,gender,password,age,city,is_married}=state
+    const {name,email,gender,password}=state
     const BaseUrl =process.env.REACT_APP_URL;
     const handlesubmit=(e)=>{
      e.preventDefault()
@@ -76,9 +61,6 @@ export const SignupPage = () => {
            Email :- <input type="text"  onChange={(e)=>dispatch({type:"Email",payload:e.target.value})} name='email' value={email} placeholder='email' /> <br />
            Password :- <input type="text"  onChange={(e)=>dispatch({type:"Password",payload:e.target.value})} name='password' value={password} placeholder='Password' /> <br />
            Gender:- <input type="text"  onChange={(e)=>dispatch({type:"Gender",payload:e.target.value})} name='gender' value={gender} placeholder='Gender' /> <br />
-           Age:- <input type="text"  onChange={(e)=>dispatch({type:"Age",payload:e.target.value})} name='age' value={age} placeholder='age' /> <br />
-           City:- <input type="text"  onChange={(e)=>dispatch({type:"City",payload:e.target.value})} name='city' value={city} placeholder='city' /> <br />
-           married:- <input type="text"  onChange={(e)=>dispatch({type:"is_married",payload:e.target.value})} name='is_married' value={is_married} placeholder='is_married' /> <br />
            <input type="submit" /> 
         </form>
     </div>
